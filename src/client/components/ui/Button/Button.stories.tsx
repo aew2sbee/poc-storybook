6cf3fb12c4c84e952b8388c5cafcb51a39717cce
Furuta Naoki <naoki.furuta@japan-systems.co.jp>
2026-01-27 17:18:46 +0900

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- src/client/components/ui/Button/Button.stories.tsx
+++ src/client/components/ui/Button/Button.stories.tsx
@@ -3,7 +3,7 @@ import { fn } from 'storybook/test';
 import { Button } from '.';
 
 const meta = {
-  title: "Example/Button",
+  title: "Sample/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
